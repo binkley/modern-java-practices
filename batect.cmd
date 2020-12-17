fbe8613b9adc35855eff72d54c1a2c58e47f88ae
Brian Oxley <boxley@thoughtworks.com>
2020-12-17 10:22:46 -0600

    Bump Batect
    Currently failing due to a CVE in Guava used by Gradle's own Checkstyle
    plugin.

--- batect.cmd
+++ batect.cmd
@@ -6,7 +6,7 @@ rem For more information, visit https://github.com/batect/batect.
 
 setlocal EnableDelayedExpansion
 
-set "version=0.64.0"
+set "version=0.64.1"
 
 if "%BATECT_CACHE_DIR%" == "" (
     set "BATECT_CACHE_DIR=%USERPROFILE%\.batect\cache"
@@ -22,7 +22,7 @@ $ErrorActionPreference = 'Stop'^
 
 ^
 
-$Version='0.64.0'^
+$Version='0.64.1'^
 
 ^
 
@@ -48,7 +48,7 @@ $UrlEncodedVersion = [Uri]::EscapeDataString($Version)^
 
 $DownloadUrl = getValueOrDefault $env:BATECT_DOWNLOAD_URL "$DownloadUrlRoot/$UrlEncodedVersion/bin/batect-$UrlEncodedVersion.jar"^
 
-$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM '7c7969d05ab8f751cbed24c462fac7e49deb732de4de70bcbfb660c23757e0f0'^
+$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM '132172733acb2923a7cc4f373eee2f4eb4a06454e09cc8dceee569355b960b7a'^
 
 ^
 
