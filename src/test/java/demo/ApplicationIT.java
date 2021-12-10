be8bf457260ce9cabdc6cc2b1e01fe309e81acf2
Brian Oxley <boxley@thoughtworks.com>
2021-12-10 11:46:48 +0800

    Cleaner

--- src/test/java/demo/ApplicationIT.java
+++ src/test/java/demo/ApplicationIT.java
@@ -10,9 +10,7 @@ public class ApplicationIT {
     /** <strong>Use case</strong>: the application runs normally. */
     @Test
     public void shouldRun() throws Exception {
-        final var out = tapSystemOutNormalized(() -> {
-            main("Hello", "world!");
-        });
+        final var out = tapSystemOutNormalized(Application::main);
 
         assertThat(out).isEqualTo("TheFoo(label=I AM FOOCUTUS OF BORG)\n");
     }
