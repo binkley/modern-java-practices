edaa10a94a845c41b7021212662ae63856994d24
Brian Oxley <boxley@thoughtworks.com>
2021-12-11 10:46:23 +0800

    Easier comparisons between Gradle and Maven

--- compare-tooling-versions.sh
+++ compare-tooling-versions.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-sed -n 's,^\(.*\)Version=\(.*\),\1 \2,p' < gradle.properties 
-echo
-sed -n 's, *<\([^.]*\)\.version>\([^<]*\)</[^.]*\.version>,\1 \2,p' < pom.xml 
+echo "LEFT IS Gradle; RIGHT IS Maven"
+diff \
+    <(sed -n 's,^\(.*\)Version=\(.*\),\1 \2,p' < gradle.properties | tr '[:upper:]' '[:lower:]') \
+    <(sed -n 's, *<\([^.]*\)\.version>\([^<]*\)</[^.]*\.version>,\1 \2,p' < pom.xml | tr '[:upper:]' '[:lower:]')
