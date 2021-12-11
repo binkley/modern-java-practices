50db35b3d45b2fe08707452a01cd4d70bbc68701
Brian Oxley <boxley@thoughtworks.com>
2021-12-11 11:06:36 +0800

    More useful comparisons between Gradle and Maven
    This script has more work to make it truly valuable.  For now it's
    enough to eyeball things.  It is utterly uneeded for projects (they'd
    pick between Gradle and Maven, not both).

--- compare-tooling-versions.sh
+++ compare-tooling-versions.sh
@@ -2,5 +2,9 @@
 
 echo "LEFT IS Gradle; RIGHT IS Maven"
 diff \
-    <(sed -n 's,^\(.*\)Version=\(.*\),\1 \2,p' < gradle.properties | tr '[:upper:]' '[:lower:]') \
-    <(sed -n 's, *<\([^.]*\)\.version>\([^<]*\)</[^.]*\.version>,\1 \2,p' < pom.xml | tr '[:upper:]' '[:lower:]')
+    <(sed -n 's,^\(.*\)Version=\(.*\),\1 \2,p' < gradle.properties \
+    | tr '[:upper:]' '[:lower:]' \
+    | tr -d -) \
+    <(sed -n 's, *<\([^.]*\)\.version>\([^<]*\)</[^.]*\.version>,\1 \2,p' < pom.xml \
+    | tr '[:upper:]' '[:lower:]' \
+    | tr -d -)
