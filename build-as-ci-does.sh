2567130ec6012065785c15b91d5597e649a44c42
Brian Oxley <boxley@thoughtworks.com>
2022-02-14 09:53:31 +0800

    Formatting

--- build-as-ci-does.sh
+++ build-as-ci-does.sh
@@ -1,3 +1,6 @@
 #/bin/sh
 
-./batect build-with-gradle && ./batect run-with-gradle && ./batect build-with-maven && ./batect run-with-maven
+./batect build-with-gradle && \
+    ./batect run-with-gradle &&
+    ./batect build-with-maven &&
+    ./batect run-with-maven
