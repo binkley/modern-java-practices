d327eac9181d6cf8bf7ce02ccc9712e59a3112ad
sergeybukharov <bukharovsi@gmail.com>
2022-03-21 23:26:36 +0800

    PMD rules enabled
    PMD requires explicitly set witch rules should be followed. Also, it is quite handy to create a custom-ruleset and configure build-in PMD rules.
    Also, due to PMD rules were switched off the commit fixes some PMD errors and supress others :)

--- src/integrationTest/java/demo/ApplicationTest.java
+++ src/integrationTest/java/demo/ApplicationTest.java
@@ -6,10 +6,20 @@ import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOutNor
 import static demo.Application.main;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ApplicationTest {
-    /** <strong>Use case</strong>: the application runs normally. */
+/**
+ * Just an example of Unit test.
+ */
+@SuppressWarnings({
+        "PMD.AtLeastOneConstructor",
+        "PMD.SignatureDeclareThrowsException"
+})
+class ApplicationTest {
+
+    /**
+     * <strong>Use case</strong>: the application runs normally.
+     **/
     @Test
-    public void shouldRun() throws Exception {
+    void shouldRun() throws Exception {
         final var out = tapSystemOutNormalized(() -> {
             main("Hello", "world!");
         });
