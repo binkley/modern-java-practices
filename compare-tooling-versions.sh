5559a61a01da8fd4ffe630a9fab4e3b213bcb894
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2022-10-26 07:54:30 -0500

    Increase readability

--- compare-tooling-versions.sh
+++ compare-tooling-versions.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# TODO: Assume GNU diff with colorization: how to present with old diff?
+# TODO: Treat plugins as one-offs, and do not compare between Gradle and Maven
+
 # Challenging to correctly and fully compare Gradle and Maven builds
 # For example, sometimes a plugin keeps in version sync upstream, sometimes
 # the plugins for the same feature come from different sources with different
@@ -9,7 +12,7 @@
 # To try other diff options, pass them on the command line to the script
 # By default, this script adds color
 
-echo "LEFT IS Gradle; RIGHT IS Maven"
+echo "LEFT is Gradle; RIGHT is Maven"
 diff --color=auto "$@" \
     <(sed -n 's,^\(.*\)Version=\(.*\),\1 \2,p' < gradle.properties \
     | tr '[:upper:]' '[:lower:]' \
