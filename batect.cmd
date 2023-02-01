6592ce025ebe30aa9f6e728e203534e39555108d
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-02-01 06:09:15 -0600

    Bump Batect

--- batect.cmd
+++ batect.cmd
@@ -6,7 +6,7 @@ rem For more information, visit https://github.com/batect/batect.
 
 setlocal EnableDelayedExpansion
 
-set "version=0.83.2"
+set "version=0.83.4"
 
 if "%BATECT_CACHE_DIR%" == "" (
     set "BATECT_CACHE_DIR=%USERPROFILE%\.batect\cache"
@@ -22,7 +22,7 @@ $ErrorActionPreference = 'Stop'^
 
 ^
 
-$Version='0.83.2'^
+$Version='0.83.4'^
 
 ^
 
@@ -48,7 +48,7 @@ $UrlEncodedVersion = [Uri]::EscapeDataString($Version)^
 
 $DownloadUrl = getValueOrDefault $env:BATECT_DOWNLOAD_URL "$DownloadUrlRoot/$UrlEncodedVersion/batect-$UrlEncodedVersion.jar"^
 
-$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM '67b462c6424b77e87fc200c656acc60a913aa2fe397b843aa84e3dcda7c6f626'^
+$ExpectedChecksum = getValueOrDefault $env:BATECT_DOWNLOAD_CHECKSUM '9e82ced1ea66be73a3fb0e50a091c41b4cdd028b87de9f1725147167b5beaacf'^
 
 ^
 
