2629a11e3819b3df103ed08067b095f4205c3903
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-03-02 10:28:47 -0600

    Run as CI does for #337

--- build-as-ci-does.sh
+++ build-as-ci-does.sh
@@ -1,6 +1,15 @@
 #/usr/bin/env bash
 
+if ! command -v earthly >/dev/null; then
+    echo "$0: No Earthly found in PATH"
+    exit 2
+fi
+
 ./batect build-with-gradle && \
     ./batect -o quiet run-with-gradle &&
+    earthly +build-with-gradle &&
+    earthly +run-with-gradle &&
     ./batect build-with-maven &&
-    ./batect -o quiet run-with-maven
+    ./batect -o quiet run-with-maven &&
+    earthly +build-with-maven &&
+    earthly +run-with-maven
