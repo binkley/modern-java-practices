1cdf7ad20a3486741408887657e758d7f1a58a04
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-10-24 07:55:06 -0500

    Nicer output

--- build-as-ci-does.sh
+++ build-as-ci-does.sh
@@ -5,11 +5,33 @@ if ! command -v earthly >/dev/null; then
     exit 2
 fi
 
-./batect build-with-gradle && \
-    ./batect -o quiet run-with-gradle &&
-    earthly +build-with-gradle &&
-    earthly +run-with-gradle &&
-    ./batect build-with-maven &&
-    ./batect -o quiet run-with-maven &&
-    earthly +build-with-maven &&
-    earthly +run-with-maven
+set -e
+
+# Provide nicer console output via formatting and color
+# See https://github.com/binkley/shell/blob/master/color/color.sh
+printf -v preset "\e[0m"
+printf -v pbold "\e[1m"
+
+echo "${preset}"
+echo "${pbold}BUILD WITH GRADLE UNDER BATECT${preset}"
+./batect -o quiet build-with-gradle 
+echo "${pbold}RUN WITH GRADLE UNDER BATECT${preset}" 
+./batect -o quiet run-with-gradle 
+
+echo 
+echo "${pbold}BUILD WITH MAVEN UNDER BATECT${preset}" 
+./batect -o quiet build-with-maven 
+echo "${pbold}RUN WITH MAVEN UNDER BATECT${preset}" 
+./batect -o quiet run-with-maven 
+
+echo 
+echo "${pbold}BUILD WITH GRADLE UNDER EARTHLY${preset}" 
+earthly +build-with-gradle 
+echo "${pbold}RUN WITH GRADLE UNDER EARTHLY${preset}" 
+earthly +run-with-gradle 
+
+echo 
+echo "${pbold}BUILD WITH MAVEN UNDER EARTHLY${preset}" 
+earthly +build-with-maven 
+echo "${pbold}RUN WITH MAVEN UNDER EARTHLY${preset}" 
+earthly +run-with-maven
