55be827b24285e30cd3f8bfcaae2081dfa2e5c1b
Sam Gammon <sam@elide.ventures>
2024-01-06 15:37:07 -0800

    feat: earthly caching, reusable ci workflows
    - feat: enable earthly remote caching via github packages
    - feat: enable step skipping in ci (earthly)
    - feat: refactor builds into reusable workflows
    - chore: optimize caching with gha cache
    - chore: upgrade most actions
    - chore: update `build-as-ci-does.sh`
    
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- Earthfile
+++ Earthfile
@@ -2,7 +2,8 @@ VERSION 0.7
 FROM eclipse-temurin:21.0.1_12-jdk-jammy
 WORKDIR /code
 
-build-with-gradle:
+gradle-commons:
+    ARG builder
     COPY gradlew .
     COPY gradle gradle
     COPY gradle.properties .
@@ -10,20 +11,29 @@ build-with-gradle:
     COPY build.gradle .
     COPY config config
     COPY src src
-    RUN ./gradlew clean build --no-configuration-cache
+    RUN --mount type=cache,target=.gradle
+    RUN --mount type=cache,target=~/.gradle
+    RUN --secret BUILDLESS_APIKEY ./gradlew dependencies
+    SAVE IMAGE --push "$builder"
+
+build-with-gradle:
+    FROM +gradle-commons
+    RUN --secret BUILDLESS_APIKEY ./gradlew clean build --no-configuration-cache
 
 run-with-gradle:
     FROM +build-with-gradle
     COPY run-with-gradle.sh .
-    RUN ./run-with-gradle.sh
+    RUN --secret BUILDLESS_APIKEY ./run-with-gradle.sh
 
 build-with-maven:
+    ARG builder
     COPY mvnw .
     COPY .mvn .mvn
     COPY pom.xml .
     COPY config config
     COPY src src
     RUN ./mvnw clean verify
+    SAVE IMAGE --push "$builder"
 
 run-with-maven:
     FROM +build-with-maven
--- build-as-ci-does.sh
+++ build-as-ci-does.sh
@@ -13,25 +13,55 @@ printf -v preset "\e[0m"
 printf -v pbold "\e[1m"
 
 echo "${preset}"
-echo "${pbold}BUILD WITH GRADLE UNDER BATECT${preset}"
-./batect -o quiet build-with-gradle 
-echo "${pbold}RUN WITH GRADLE UNDER BATECT${preset}" 
-./batect -o quiet run-with-gradle 
+#echo "${pbold}BUILD WITH GRADLE UNDER BATECT${preset}"
+#./batect -o quiet build-with-gradle
+#echo "${pbold}RUN WITH GRADLE UNDER BATECT${preset}"
+#./batect -o quiet run-with-gradle
 
 echo 
-echo "${pbold}BUILD WITH MAVEN UNDER BATECT${preset}" 
-./batect -o quiet build-with-maven 
-echo "${pbold}RUN WITH MAVEN UNDER BATECT${preset}" 
-./batect -o quiet run-with-maven 
+#echo "${pbold}BUILD WITH MAVEN UNDER BATECT${preset}"
+#./batect -o quiet build-with-maven
+#echo "${pbold}RUN WITH MAVEN UNDER BATECT${preset}"
+#./batect -o quiet run-with-maven
 
 echo 
 echo "${pbold}BUILD WITH GRADLE UNDER EARTHLY${preset}" 
-earthly +build-with-gradle 
-echo "${pbold}RUN WITH GRADLE UNDER EARTHLY${preset}" 
-earthly +run-with-gradle 
+earthly \
+  --secret BUILDLESS_APIKEY \
+  --remote-cache=ghcr.io/sgammon/modern-java-practices/builder/gradle:latest \
+  --push \
+  --ci \
+  --auto-skip \
+  +build-with-gradle \
+  --builder=ghcr.io/sgammon/modern-java-practices/builder/gradle:latest
+
+echo "${pbold}RUN WITH GRADLE UNDER EARTHLY${preset}"
+earthly \
+  --secret BUILDLESS_APIKEY \
+  --remote-cache=ghcr.io/sgammon/modern-java-practices/builder/gradle:latest \
+  --push \
+  --ci \
+  --auto-skip \
+  +run-with-gradle \
+  --builder=ghcr.io/sgammon/modern-java-practices/builder/gradle:latest
 
 echo 
 echo "${pbold}BUILD WITH MAVEN UNDER EARTHLY${preset}" 
-earthly +build-with-maven 
+earthly \
+  --secret BUILDLESS_APIKEY \
+  --remote-cache=ghcr.io/sgammon/modern-java-practices/builder/maven:latest \
+  --push \
+  --ci \
+  --auto-skip \
+  +build-with-maven \
+  --builder=ghcr.io/sgammon/modern-java-practices/builder/maven:latest
+
 echo "${pbold}RUN WITH MAVEN UNDER EARTHLY${preset}" 
-earthly +run-with-maven
+earthly \
+  --secret BUILDLESS_APIKEY \
+  --remote-cache=ghcr.io/sgammon/modern-java-practices/builder/maven:latest \
+  --push \
+  --ci \
+  --auto-skip \
+  +run-with-maven \
+  --builder=ghcr.io/sgammon/modern-java-practices/builder/maven:latest
