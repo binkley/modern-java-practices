14da9ba8e1f98714ac0e19267350b807a29838e2
Sam Gammon <sam@elide.ventures>
2024-01-06 19:30:27 -0800

    fixup: fixes for gradle builds, add direct maven build
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- run-with-gradle.sh
+++ run-with-gradle.sh
@@ -83,7 +83,7 @@ function jar-outdated() {
 
 function rebuild-if-needed() {
     if jar-outdated || build-config-outdated-gradle; then
-        ./gradlew --warning-mode=all --no-configuration-cache jar
+        ./gradlew --warning-mode=none --no-configuration-cache --no-daemon jar
     fi
 }
 
