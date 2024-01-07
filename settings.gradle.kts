3671ddd807f6bddb424faa761779ad8691607370
Sam Gammon <sam@elide.ventures>
2024-01-06 21:21:37 -0800

    feat: enhanced user data for gradle build scans
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
     repositories {
         maven("https://gradle.pkg.st")
@@ -7,6 +9,7 @@ pluginManagement {
 plugins {
     id("build.less") version "1.0.0-rc2"
     id("com.gradle.enterprise") version "3.16.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.12.1"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
