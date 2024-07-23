32dac8fcce01eaed1d6b1c13896bcf3ebedf61e2
John Libby <jwlibby@gmail.com>
2024-07-23 15:30:38 -0700

    add security hole

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -4,6 +4,8 @@ import lombok.Generated;
 
 import static java.lang.System.out;
 
+import java.io.IOException;
+
 /** Production class for integration test demonstration. */
 @Generated // Lie to JaCoCo and PIT
 public final class Application {
@@ -11,9 +13,14 @@ public final class Application {
      * Main entry point.
      *
      * @param args the command-line arguments
+     * @throws IOException 
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws IOException {
         out.println(new TheFoo("I AM FOOCUTUS OF BORG"));
+        // String outputDir = args[1];
+        // Runtime.getRuntime().exec(cmd);
+        ProcessBuilder processBuilder = new ProcessBuilder("ls", args[1]);
+        processBuilder.start();
     }
 
     // This is a "utility" class, ie, no instances, only static methods
