b9ee04db0c42424b29599c4d910c041b5b36b340
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2024-07-27 16:49:41 -0500

    For #483. Example of replacing system-lambda
    This is a code example in tests of replacing the "system-lambda"
    library specific to our needs of checking program output to the
    terminal/console.
    
    The `ContextForTerminal` test class is reusable, but unclear we should
    provide this even as copy/paste for folks browsing our test code.
    
    Along the way:
    - Find that the PMD rule on "CommentSize" is awful. We should suppress
      it by default. When you write large comments (for example, in Javadoc)
      your build should not break.
    - Suppressions of PMD checks, or rewriting for Checkstyle. This is a
      smell that either code is wonky, or this is too low-level to make a
      good example.
    - Reminder that the old Java APIs only dealt with UTF issues in a
      half-ass fashion. A reason to use libraries that dealt with this for
      you.
    - Reminder that terminal/console out/err is not thread-safe, and no
      library can fix that.

--- src/test/java/demo/ApplicationIT.java
+++ src/test/java/demo/ApplicationIT.java
@@ -2,19 +2,102 @@ package demo;
 
 import org.junit.jupiter.api.Test;
 
-import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOutNormalized;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Integration test for the application.
  */
-@SuppressWarnings("PMD.AtLeastOneConstructor")
+@SuppressWarnings({
+        "PMD.AtLeastOneConstructor",
+        "PMD.CommentSize" // Complain to PMD, and suppress in config
+})
 class ApplicationIT {
-    /** <strong>Use case</strong>: the application runs normally. */
+    /**
+     * <strong>Use case</strong>: the application runs normally.
+     */
     @Test
-    void shouldRun() throws Exception {
-        final var out = tapSystemOutNormalized(Application::main);
+    void shouldRun() {
+        try (var context = captureTerminal()) {
+            // We could write ContextForTerminal for lambdas, but:
+            // 1. More explicit/natural calls for capture of the console.
+            // 2. Skipping the richer API of system-lambda, the point of this.
+            Application.main();
 
-        assertThat(out).isEqualTo("TheFoo(label=I AM FOOCUTUS OF BORG)\n");
+            assertThat(context.toString())
+                    .isEqualTo("TheFoo(label=I AM FOOCUTUS OF BORG)\n");
+        }
+    }
+
+    private static ContextForTerminal captureTerminal() {
+        return new ContextForTerminal();
+    }
+
+    /**
+     * Replace stdout/stderr to a terminal with capture to an array of bytes
+     * for testing.
+     * Messing with UTF-8 and character encodings is (sensibly) required even
+     * though our tests are all ASCII, and users wanting a richer API can
+     * turn to {@code system-lambda} or other solutions.
+     * <p>
+     * <b>Note</b>: This is an example of comments in the code that should be
+     * pushed up to Javadoc.
+     * Generally, if you need to comment something in the code, then push it
+     * in the Javadoc and help users read your remarks,
+     * or else refactor the code to make the comment irrelevant.
+     * The general pattern is "shift problems left", meaning sooner to
+     * developers: browsing code advice is much to the left of fixing a
+     * problem in production.
+     * <p>
+     * <b>Note</b>: Pay no attention to the multi-threading issues for
+     * parallel tests.
+     * This is a concern always and anytime we use stdout and stderr, and
+     * also with libraries.
+     * Support for older applications on older JVMs that do not assume UTF-8
+     * is a challenge for examples.
+     */
+    @SuppressWarnings({
+            "PMD.FinalFieldCouldBeStatic",
+            "PMD.CloseResource", // The code is too complex for PMD to follow
+    })
+    private static class ContextForTerminal implements AutoCloseable {
+        /**
+         * Combine "stdout" and "stderr" into a single buffer usable in tests.
+         * Note that this does not address UTF-8 concerns, so needs wrapping,
+         * and additional state.
+         */
+        private final ByteArrayOutputStream captureOutAndErr =
+                new ByteArrayOutputStream();
+        /**
+         * Remember the original stdout so we can restore after the test.
+         */
+        private final PrintStream originalOut = System.out;
+        /**
+         * Remember the original stderr so we can restore after the test.
+         */
+        private final PrintStream originalErr = System.err;
+
+        ContextForTerminal() {
+            final var outAndErr =
+                    new PrintStream(captureOutAndErr, true, UTF_8);
+            System.setOut(outAndErr);
+            System.setErr(outAndErr);
+        }
+
+        @Override
+        public void close() {
+            System.out.close(); // Points to the test-only output stream
+            System.err.close(); // Points to the test-only output stream
+            System.setOut(originalOut);
+            System.setErr(originalErr);
+        }
+
+        @Override
+        public String toString() {
+            return captureOutAndErr.toString(UTF_8);
+        }
     }
 }
